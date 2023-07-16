9f46cbc7dbac6b87c1ebc67b3c8587f4e746d08c
Stenstromen <104073740+Stenstromen@users.noreply.github.com>
2023-07-17 00:31:11 +0200

    WIP

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ import (
 	"github.com/stenstromen/ncregistry/utils"
 )
 
-func promptSelect(label string, items []string) (string, error) {
+func promptSelect(label string, items []string) (int, string, error) {
 	prompt := promptui.Select{
 		HideSelected: true,
 		Label:        label,
 		Items:        items,
+		Size:         30,
 		Templates: &promptui.SelectTemplates{
 			Active:   `👉 {{ . | cyan | bold }}`,
 			Inactive: `   {{ . | cyan }}`,
@@ -26,9 +27,9 @@ func promptSelect(label string, items []string) (string, error) {
 			Help:     `{{ "Use ↑/↓ to move and Enter to select" | bold }}`,
 		},
 	}
-	_, result, err := prompt.Run()
+	i, result, err := prompt.Run()
 
-	return result, err
+	return i, result, err
 }
 
 func promptInput(label string, mask rune) (string, error) {
@@ -92,19 +93,7 @@ func removeRegistry() {
 		return
 	}
 
-	prompt := promptui.Select{
-		HideSelected: true,
-		Label:        "Select Registry",
-		Items:        urls,
-		Templates: &promptui.SelectTemplates{
-			Active:   `👉 {{ . | cyan | bold }}`,
-			Inactive: `   {{ . | cyan }}`,
-			Selected: `{{ "✔" | green | bold }} {{ "Selected Option:" | bold }} {{ . | cyan }}`,
-			Help:     `{{ "Remove registry" | bold }}`,
-		},
-	}
-
-	i, _, err := prompt.Run()
+	i, _, err := promptSelect("Select Registry", urls)
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 		return
@@ -121,14 +110,181 @@ func removeRegistry() {
 	}
 }
 
+func connectToRegistry() {
+	utils.ClearTerminal()
+	var urls []string
+	urls = append(urls, "../")
+	urls[0] = "../"
+	for _, entry := range config.Config.Entries {
+		urls = append(urls, strings.Split(entry.URL, "://")[1])
+	}
+
+	if (len(urls)) == 1 {
+		fmt.Println("No registries found. Please add a registry first.")
+		time.Sleep(2 * time.Second)
+		return
+	}
+Registrylist:
+	i, result, err := promptSelect("Select Registry", urls)
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	if result == "../" {
+		return
+	}
+
+	selectedRegistry := config.Config.Entries[i-1]
+
+	repositories, err := registry.GetRepositories(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password)
+	if err != nil {
+		fmt.Println("Failed to fetch repositories:", err)
+		return
+	}
+
+	repoItems := make([]string, len(repositories.Repositories)+1)
+	repoItems[0] = "../"
+	copy(repoItems[1:], repositories.Repositories)
+
+Repolist:
+	_, result, err = promptSelect("Select Repository", repoItems)
+
+	if result == "../" {
+		goto Registrylist
+	}
+
+	selectedRepository := result
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	tags, err := registry.GetTags(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, result)
+	if err != nil {
+		fmt.Println("Failed to fetch tags:", err)
+		return
+	}
+
+	if (len(tags.Tags)) == 0 {
+		fmt.Println("No tags found for this repository.")
+		time.Sleep(2 * time.Second)
+		return
+	}
+
+	tagInfos := make([]types.TagInfo, len(tags.Tags))
+
+	for i, tag := range tags.Tags {
+		manifest, err := registry.GetManifest(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, result, tag)
+		digest := manifest.Config.Digest
+		if err != nil {
+			if strings.Contains(err.Error(), "MANIFEST_UNKNOWN") {
+				tagInfos[i] = types.TagInfo{
+					Name: tag + " (empty)",
+					Date: "N/A",
+					Size: 0,
+				}
+			} else {
+				fmt.Println("Failed to fetch manifest:", err)
+				return
+			}
+		} else {
+			totalSize := manifest.Config.Size
+			for _, layer := range manifest.Layers {
+				totalSize += layer.Size
+			}
+
+			blobResp, err := registry.GetBlob(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, result, digest)
+			if err != nil {
+				fmt.Println("Failed to fetch blob:", err)
+				return
+			}
+
+			daysAgo, err := utils.ConvertToDaysAgo(blobResp.Created)
+			if err != nil {
+				fmt.Println("Failed to convert timestamp:", err)
+				return
+			}
+
+			tagInfos[i] = types.TagInfo{
+				Name: tag,
+				Date: daysAgo,
+				Size: totalSize,
+			}
+		}
+	}
+
+	tagItems := make([]string, len(tagInfos)+1)
+	tagItems[0] = "../"
+	for i, info := range tagInfos {
+		tagItems[i+1] = fmt.Sprintf("%s (Created %s) %s", info.Name, info.Date, utils.FormatBytes(info.Size))
+	}
+Taglist:
+	utils.ClearTerminal()
+	_, result, err = promptSelect("Select Tag", tagItems)
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	if result == "../" {
+		goto Repolist
+	}
+
+	selectedTag := result[:strings.Index(result, " (")]
+
+	_, result, err = promptSelect("Select Action", []string{"../", "Pull", "Delete"})
+	if err != nil || result == "../" {
+		goto Taglist
+	}
+
+	switch result {
+	case "Pull":
+		fmt.Println("Pulling...")
+		err = registry.DockerPull(strings.Split(selectedRegistry.URL, "://")[1], selectedRepository, selectedTag)
+		if err != nil {
+			fmt.Println("Failed to pull image:", err)
+			return
+		}
+		goto Taglist
+
+	case "Delete":
+		_, result, err = promptSelect("Are you sure?", []string{"Yes", "No"})
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+
+		if result == "Yes" {
+			manifest, err := registry.GetManifest(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, selectedRepository, selectedTag)
+			digest := manifest.Config.Digest
+			if err != nil {
+				fmt.Println("Failed to fetch manifest:", err)
+				return
+			}
+
+			fmt.Println("Deleting...")
+			err = registry.DeleteManifest(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, selectedRepository, digest)
+			if err != nil {
+				fmt.Println("Failed to delete manifest:", err)
+				return
+			}
+
+			fmt.Println("Delete successful")
+			time.Sleep(2 * time.Second)
+			goto Taglist
+		}
+	}
+}
+
 func main() {
 	config.InitConfig()
 
 	for {
-
 		utils.ClearTerminal()
 
-		result, err := promptSelect("Main Menu", []string{"Add registry", "Remove registry", "Connect to registry", "Exit"})
+		_, result, err := promptSelect("Main Menu", []string{"Add registry", "Remove registry", "Connect to registry", "Exit"})
 		if err != nil {
 			fmt.Printf("Prompt failed %v\n", err)
 			return
@@ -137,239 +293,10 @@ func main() {
 		switch result {
 		case "Add registry":
 			addRegistry()
-
 		case "Remove registry":
 			removeRegistry()
-
 		case "Connect to registry":
-			utils.ClearTerminal()
-			var urls []string
-			urls = append(urls, "../")
-			urls[0] = "../"
-			for _, entry := range config.Config.Entries {
-				urls = append(urls, strings.Split(entry.URL, "://")[1])
-			}
-
-			if (len(urls)) == 1 {
-				fmt.Println("No registries found. Please add a registry first.")
-				time.Sleep(2 * time.Second)
-				continue
-			}
-		Registrylist:
-			prompt := promptui.Select{
-				Label:        "Select Registry",
-				HideSelected: true,
-				Items:        urls,
-				Templates: &promptui.SelectTemplates{
-					Active:   `👉 {{ . | cyan | bold }}`,
-					Inactive: `   {{ . | cyan }}`,
-					Selected: `{{ "✔" | green | bold }} {{ "Selected Option:" | bold }} {{ . | cyan }}`,
-					Help:     `{{ "Connect to registry" | bold }}`,
-				},
-			}
-
-			i, result, err := prompt.Run()
-			if err != nil {
-				fmt.Printf("Prompt failed %v\n", err)
-				return
-			}
-
-			if result == "../" {
-				continue
-			}
-
-			selectedRegistry := config.Config.Entries[i-1]
-
-			fmt.Printf("Repositories for %s:\n", strings.Split(selectedRegistry.URL, "://")[1])
-			repositories, err := registry.GetRepositories(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password)
-			if err != nil {
-				fmt.Println("Failed to fetch repositories:", err)
-				return
-			}
-
-			repoItems := make([]string, len(repositories.Repositories)+1)
-			repoItems[0] = "../"
-			copy(repoItems[1:], repositories.Repositories)
-
-		Repolist:
-			prompt = promptui.Select{
-				Label:        "Select Repository",
-				HideHelp:     true,
-				HideSelected: true,
-				Items:        repoItems,
-				Size:         30,
-				Templates: &promptui.SelectTemplates{
-					Active:   `👉 {{ . | cyan | bold }}`,
-					Inactive: `   {{ . | cyan }}`,
-					Selected: `{{ "✔" | green | bold }} {{ "Selected Option:" | bold }} {{ . | cyan }}`,
-				},
-			}
-
-			_, result, err = prompt.Run()
-
-			if result == "../" {
-				goto Registrylist
-			}
-
-			selectedRepository := result
-
-			if err != nil {
-				fmt.Printf("Prompt failed %v\n", err)
-				return
-			}
-
-			tags, err := registry.GetTags(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, result)
-			if err != nil {
-				fmt.Println("Failed to fetch tags:", err)
-				return
-			}
-
-			if (len(tags.Tags)) == 0 {
-				fmt.Println("No tags found for this repository.")
-				time.Sleep(2 * time.Second)
-				continue
-			}
-
-			tagInfos := make([]types.TagInfo, len(tags.Tags))
-
-			for i, tag := range tags.Tags {
-				manifest, err := registry.GetManifest(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, result, tag)
-				digest := manifest.Config.Digest
-				if err != nil {
-					// Check if error is because manifest doesn't exist
-					if strings.Contains(err.Error(), "MANIFEST_UNKNOWN") {
-						tagInfos[i] = types.TagInfo{
-							Name: tag + " (empty)",
-							Date: "N/A",
-							Size: 0,
-						}
-					} else {
-						fmt.Println("Failed to fetch manifest:", err)
-						return
-					}
-				} else {
-					// Calculate total size
-					totalSize := manifest.Config.Size
-					for _, layer := range manifest.Layers {
-						totalSize += layer.Size
-					}
-
-					blobResp, err := registry.GetBlob(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, result, digest)
-					if err != nil {
-						fmt.Println("Failed to fetch blob:", err)
-						return
-					}
-
-					daysAgo, err := utils.ConvertToDaysAgo(blobResp.Created)
-					if err != nil {
-						fmt.Println("Failed to convert timestamp:", err)
-						return
-					}
-
-					tagInfos[i] = types.TagInfo{
-						Name: tag,
-						Date: daysAgo,
-						Size: totalSize, // Add size here
-					}
-				}
-			}
-
-			tagItems := make([]string, len(tagInfos)+1)
-			tagItems[0] = "../"
-			for i, info := range tagInfos {
-				tagItems[i+1] = fmt.Sprintf("%s (Created %s) %s", info.Name, info.Date, utils.FormatBytes(info.Size))
-			}
-		Taglist:
-			utils.ClearTerminal()
-			prompt = promptui.Select{
-				Label:        "Select Tag",
-				HideHelp:     true,
-				HideSelected: true,
-				Items:        tagItems,
-				Size:         100,
-				Templates: &promptui.SelectTemplates{
-					Active:   `👉 {{ . | cyan | bold }}`,
-					Inactive: `   {{ . | cyan }}`,
-					Selected: `{{ "✔" | green | bold }} {{ "Selected Option:" | bold }} {{ . | cyan }}`,
-				},
-			}
-			_, result, err = prompt.Run()
-			if err != nil {
-				fmt.Printf("Prompt failed %v\n", err)
-				return
-			}
-
-			if result == "../" {
-				goto Repolist
-			}
-
-			selectedTag := result[:strings.Index(result, " (")]
-
-			prompt = promptui.Select{
-				Label:        "Select Action",
-				HideHelp:     true,
-				HideSelected: true,
-				Items:        []string{"../", "Pull", "Delete"},
-				Templates: &promptui.SelectTemplates{
-					Active:   `👉 {{ . | cyan | bold }}`,
-					Inactive: `   {{ . | cyan }}`,
-					Selected: `{{ "✔" | green | bold }} {{ "Selected Option:" | bold }} {{ . | cyan }}`,
-				},
-			}
-
-			_, result, err = prompt.Run()
-			if err != nil || result == "../" {
-				goto Taglist
-			}
-
-			switch result {
-			case "Pull":
-				fmt.Println("Pulling...")
-				err = registry.DockerPull(strings.Split(selectedRegistry.URL, "://")[1], selectedRepository, selectedTag)
-				if err != nil {
-					fmt.Println("Failed to pull image:", err)
-					return
-				}
-				goto Taglist
-
-			case "Delete":
-				prompt = promptui.Select{
-					Label: "Are you sure?",
-					Items: []string{"Yes", "No"},
-					Templates: &promptui.SelectTemplates{
-						Active:   `👉 {{ . | cyan | bold }}`,
-						Inactive: `   {{ . | cyan }}`,
-						Selected: `{{ "✔" | green | bold }} {{ "Selected Option:" | bold }} {{ . | cyan }}`,
-						Help:     `{{ "Press ESC to go back" | bold }}`,
-					},
-				}
-
-				_, result, err = prompt.Run()
-				if err != nil {
-					fmt.Printf("Prompt failed %v\n", err)
-					return
-				}
-
-				if result == "Yes" {
-					manifest, err := registry.GetManifest(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, selectedRepository, selectedTag)
-					digest := manifest.Config.Digest
-					if err != nil {
-						fmt.Println("Failed to fetch manifest:", err)
-						return
-					}
-
-					fmt.Println("Deleting...")
-					err = registry.DeleteManifest(selectedRegistry.URL, selectedRegistry.Username, selectedRegistry.Password, selectedRepository, digest)
-					if err != nil {
-						fmt.Println("Failed to delete manifest:", err)
-						return
-					}
-
-					fmt.Println("Delete successful")
-					time.Sleep(2 * time.Second)
-					goto Taglist
-				}
-			}
+			connectToRegistry()
 		case "Exit":
 			return
 		}
